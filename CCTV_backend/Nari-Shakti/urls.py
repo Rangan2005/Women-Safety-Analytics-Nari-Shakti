96ad79a9e8e86b675f489ddd0087c17f3c2b7abc
Subhrajyoti Basu <168283852+Rangan2005@users.noreply.github.com>
2024-10-03 18:14:31 +0530

    Delete CCTV_backend/Nari-Shakti directory

--- CCTV_backend/Nari-Shakti/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from .views import display_live_feed,live_feed
-
-urlpatterns = [
-    path('live/', display_live_feed, name='display_live_feed'),
-    path('live_feed/', live_feed, name='live_feed'),
-]
